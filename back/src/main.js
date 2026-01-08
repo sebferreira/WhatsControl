559ba23aafefd2f59e33aebee7185d1fa9415a58
sebferreira <sebastianleonelgalarza@gmail.com>
2026-01-08 18:29:54 -0300

    cambiando json

--- back/src/main.js
+++ back/src/main.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {createServer} from "http";
 import {Server} from "socket.io";
-import qrcode from "qrcode-terminal";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
